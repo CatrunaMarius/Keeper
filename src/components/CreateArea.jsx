a527f3e3d664bd037173f61f42eb5e8d75907698
Marius_haru <roberto_marius12@yahoo.com>
2020-09-07 20:35:17 +0300

    Final project

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  //========= Start Partea 1 ===========
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  // ===== partea 1 + add  setNote- ========
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+
+    event.preventDefault();
+  }
+
+  //============= End partea 1 ====================
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
